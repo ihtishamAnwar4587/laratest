4e03091ff231526359e76a22b66f645e2225fb4e
ihtisham <mihtisham4587@gmail.com>
2023-04-27 04:16:36 +0500

    task 4 has been completed

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -7,26 +7,22 @@ use Illuminate\Support\Facades\Schema;
 class CreateCinemaSchema extends Migration
 {
     /** ToDo: Create a migration that creates all tables for the following user stories
-
-    For an example on how a UI for an api using this might look like, please try to book a show at https://in.bookmyshow.com/.
-    To not introduce additional complexity, please consider only one cinema.
-
-    Please list the tables that you would create including keys, foreign keys and attributes that are required by the user stories.
-
-    ## User Stories
-
+     *
+     * For an example on how a UI for an api using this might look like, please try to book a show at https://in.bookmyshow.com/.
+     * To not introduce additional complexity, please consider only one cinema.
+     *
+     * Please list the tables that you would create including keys, foreign keys and attributes that are required by the user stories.
+     *
+     * ## User Stories
      **Movie exploration**
      * As a user I want to see which films can be watched and at what times
      * As a user I want to only see the shows which are not booked out
-
      **Show administration**
      * As a cinema owner I want to run different films at different times
      * As a cinema owner I want to run multiple films at the same time in different showrooms
-
      **Pricing**
      * As a cinema owner I want to get paid differently per show
      * As a cinema owner I want to give different seat types a percentage premium, for example 50 % more for vip seat
-
      **Seating**
      * As a user I want to book a seat
      * As a user I want to book a vip seat/couple seat/super vip/whatever
@@ -36,7 +32,82 @@ class CreateCinemaSchema extends Migration
      */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('poster_url');
+            $table->integer('duration');
+            $table->timestamps();
+        });
+
+        Schema::create('halls', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('capacity');
+            $table->timestamps();
+        });
+
+        Schema::create('shows', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('movie_id');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->unsignedBigInteger('price_id');
+            $table->unsignedBigInteger('hall_id');
+            $table->timestamps();
+
+            $table->foreign('movie_id')->references('id')->on('movies');
+            $table->foreign('hall_id')->references('id')->on('halls');
+        });
+
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('amount', 8, 2);
+            $table->timestamps();
+        });
+
+        Schema::create('seats', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('hall_id');
+            $table->boolean('is_vip')->default(false);
+            $table->boolean('is_couple')->default(false);
+            $table->boolean('is_super_vip')->default(false);
+            $table->unsignedBigInteger('price_id');
+            $table->timestamps();
+
+            $table->foreign('hall_id')->references('id')->on('halls');
+            $table->foreign('price_id')->references('id')->on('prices');
+        });
+
+        Schema::create('show_seats', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('show_id');
+            $table->unsignedBigInteger('seat_id');
+            $table->boolean('is_taken')->default(false);
+            $table->unsignedBigInteger('booked_by')->nullable();
+            $table->timestamps();
+
+            $table->foreign('show_id')->references('id')->on('shows');
+            $table->foreign('seat_id')->references('id')->on('seats');
+            $table->foreign('booked_by')->references('id')->on('users');
+        });
+
+        Schema::create('show_revenues', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('show_id');
+            $table->decimal('revenue', 8, 2);
+            $table->unsignedBigInteger('movie_id');
+            $table->timestamps();
+
+            $table->foreign('show_id')->references('id')->on('shows');
+            $table->foreign('movie_id')->references('id')->on('movies');
+        });
+
+//        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
     }
 
     /**
